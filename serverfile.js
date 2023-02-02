95b002c9f65376c2bc9bb86f377e2d84e0624cda
Abdul Kadir <ak8871639@gmail.com>
2023-02-02 17:39:08 +0530

    final commit

--- serverfile.js
+++ serverfile.js
@@ -31,7 +31,7 @@ const client= new Client(
         ssl:{rejectUnauthorized:false}
     }
 );
-client.connect(function(err,error){
+client.connect(function(res,error){
     console.log('connected!!!');
 })
 app.listen(port,()=>console.log(`Node app is listinng${port}`))
@@ -47,6 +47,7 @@ let connData={
 app.get('/svr/employees',function(req,res){
 
     let {designation,department,gender}=req.query
+    console.log(designation,department,gender)
     let sql='select * from employees'
     client.query(sql,function(err,result){
         if(err){
@@ -60,13 +61,13 @@ app.get('/svr/employees',function(req,res){
            res.send(result.rows)
           
         }
-        
     })
 })
 
 app.get('/svr/employees/department/:dept',function(req,res){
     let {dept}=req.params
     let sql='select * from employees'
+    console.log(dept)
     client.query(sql,function(err,result){
         if(err){
             res.status(404).send(err)
@@ -82,6 +83,7 @@ app.get('/svr/employees/department/:dept',function(req,res){
 })
 app.get('/svr/employees/designation/:des',function(req,res){
     let {des}=req.params
+    console.log(des)
     let sql='select * from employees'
     client.query(sql,function(err,result){
         if(err){
@@ -103,11 +105,14 @@ app.get('/svr/employees/designation/:des',function(req,res){
 
 app.post('/svr/employees',function(req,res){
     let {body}=req
+   
     let {name,department,designation,salary,gender}=body
-    let sql='INSERT INTO employees(name,department,designation,salary,gender) VALUES(?,?,?,?,?)'
+    let sql='INSERT INTO employees(name,department,designation,salary,gender) VALUES($1,$2,$3,$4,$5)'
+   
     client.query(sql,[name,department,designation,salary,gender],function(err,result){
         if(err){
-            res.send(err)
+             console.log(err)
+            res.status(404).send(err)
         }
         else{
             res.send(body)
@@ -129,11 +134,12 @@ app.put('/svr/employees/:id',function(req,res){
             res.status(404).send(err)
         }
     else{
-    let sql2='update employees set name=?,department=?,designation=?,salary=?, gender=? where id=?'
-    let index=result.findIndex(elem=>elem.id==id)
+    let sql2='update employees set name=$1,department=$2,designation=$3,salary=$4, gender=$5 where id=$6'
+    let index=result.rows.findIndex(elem=>elem.id==id)
     if(index>=0){
     client.query(sql2,[name,department,designation,salary,gender,id],function(err,result){
         if(err){
+            console.log(err)
             res.status(404).send(err)
         }
         else{
@@ -154,18 +160,21 @@ app.put('/svr/employees/:id',function(req,res){
 
 app.delete('/svr/employees/:id',function(req,res){
      let {id}=req.params
-    
+   
      let sql='SELECT * FROM employees'
      client.query(sql,function(err,result){
         if(err){
+            console.log('select err is',err)
             res.status(404).send(err)
+          
         }
         else{
             let index=result.rows.findIndex(elem=>elem.id==id)
             if(index>=0){
-               let sql2='DELETE FROM employees WHERE id=?'
-               client.query(sql2,id,function(err,result2){
+               let sql2='DELETE FROM employees WHERE id=$1 '
+               client.query(sql2,[id],function(err,result2){
                  if(err){
+                    console.log('delet err:',err)
                     res.status(404).send(err)
                  }
                  else{
